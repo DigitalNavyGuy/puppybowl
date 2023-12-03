9a75cef34e7c61c89a81d032bbbe7d2dec2423b5
ataraxygale <adisylvestro1@gmail.com>
2023-12-03 12:14:37 -0800

    BareBonesIdeas

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+// DOM refresher: these don't get called normally
+// they are ran when JS is executed and store their values to those elements
 const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
 const cohortName = '2310-FSA-ET-WEB-PT-SF';
 // Use the APIURL variable for fetch requests
-const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/players`;
 
 /**
  * It fetches all players from the API and returns them
@@ -12,93 +14,116 @@ const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
  */
 const fetchAllPlayers = async () => {
     try {
-        const response = await fetch(`${APIURL}players`);
-        const result = await response.json();
-        console.log(result);
+        // I am filling in this part - we need a stored variable that calls the API data
+        // fetchAllPlayers will only fetch all players at a base level
+        const response = await fetch(APIURL);
+
+        // Parse the response JSON and store it in a variable
+        const data = await response.json();
+
+        // Store the fetched players in a variable
+        const fetchedPlayers = data.players;
+
+        // Log the fetched players to the console (for testing)
+        console.log('Fetched Players:', fetchedPlayers);
+
+        // Check if the request was successful (status code 200)
+        if (response.ok) {
+            // Parse the response JSON and store it in a variable
+            const data = await response.json();
+
+            // Store the fetched players in a variable
+            const fetchedPlayers = data.players;
+
+            // Log the fetched players to the console (for testing)
+            console.log('Fetched Players:', fetchedPlayers);
+
+        } else {
+            // If the response status is not ok, throw an error
+            throw new Error(`Failed to fetch players. Status: ${response.status}`);
+        }
+            // Return the array of fetched players
+            return fetchedPlayers;
 
     } catch (err) {
         console.error('Uh oh, trouble fetching players!', err);
     }
+    // finally is a type of JS option where this last code will run whether something fails or not
+    // we probably don't need this but want this option available
+
 };
 
 const fetchSinglePlayer = async (playerId) => {
     try {
-        const response = await fetch(`${APIURL}players/${playerId}`);
-        const result = await response.json();
-        console.log(result);
+        // Construct the URL for fetching a single player by playerId
+        const singlePlayerURL = `${APIURL}/${playerId}`;
 
+        // Fetch the single player details from the API
+        const response = await fetch(singlePlayerURL);
+
+        // Check if the request was successful (status code 200)
+        if (response.ok) {
+            // Parse the response JSON and store it in a variable
+            const data = await response.json();
+
+            // Store the fetched player details in a variable
+            const fetchedPlayer = data.player;
+
+            // Log the fetched player details to the console (for testing)
+            console.log(`Fetched Player #${playerId}:`, fetchedPlayer);
+
+            // Return the details of the fetched player
+            return fetchedPlayer;
+        } else {
+            // If the response status is not ok, throw an error
+            throw new Error(`Failed to fetch player #${playerId}. Status: ${response.status}`);
+        }
     } catch (err) {
         console.error(`Oh no, trouble fetching player #${playerId}!`, err);
     }
 };
 
-
 const addNewPlayer = async (playerObj) => {
     try {
-        const response = await fetch(`${APIURL}players`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                name: "Smokey",
-                breed: "American Staffordshire Terrier",
-                status: "field"
-            }),
-        });
-        if(!response.ok){
-            throw new Error("Fetch failed to add new player.")
-        }
-        const result = await response.json();
-        console.log(result);
 
     } catch (err) {
         console.error('Oops, something went wrong with adding that player!', err);
     }
 };
 
-
 const removePlayer = async (playerId) => {
-    fetch(`${APIURL}players`, {
-        method: "DELETE",
-    });
     try {
-        const respponse = await fetch(`${APIURL}players/${playerId}`, {
-            method: "DELETE",
-        });
-        const result = await response.json();
-        console.log(result);
+
     } catch (err) {
         console.error(
             `Whoops, trouble removing player #${playerId} from the roster!`,
             err
-            );
-        }
-    };
+        );
+    }
+};
 
-
-    /**
-     * It takes an array of player objects, loops through them, and creates a string of HTML for each
-     * player, then adds that string to a larger string of HTML that represents all the players.
-     *
-     * Then it takes that larger string of HTML and adds it to the DOM.
-     *
-     * It also adds event listeners to the buttons in each player card.
-     *
-     * The event listeners are for the "See details" and "Remove from roster" buttons.
-     *
-     * The "See details" button calls the `fetchSinglePlayer` function, which makes a fetch request to the
-     * API to get the details for a single player.
-     *
-     * The "Remove from roster" button calls the `removePlayer` function, which makes a fetch request to
-     * the API to remove a player from the roster.
-     *
-     * The `fetchSinglePlayer` and `removePlayer` functions are defined in the
-     * @param playerList - an array of player objects
-     * @returns the playerContainerHTML variable.
-    */
-   const renderAllPlayers = (playerList) => {
-       try {
+/**
+ * It takes an array of player objects, loops through them, and creates a string of HTML for each
+ * player, then adds that string to a larger string of HTML that represents all the players.
+ *
+ * Then it takes that larger string of HTML and adds it to the DOM.
+ *
+ * It also adds event listeners to the buttons in each player card.
+ *
+ * The event listeners are for the "See details" and "Remove from roster" buttons.
+ *
+ * The "See details" button calls the `fetchSinglePlayer` function, which makes a fetch request to the
+ * API to get the details for a single player.
+ *
+ * The "Remove from roster" button calls the `removePlayer` function, which makes a fetch request to
+ * the API to remove a player from the roster.
+ *
+ * The `fetchSinglePlayer` and `removePlayer` functions are defined in the
+ * @param playerList - an array of player objects
+ * @returns the playerContainerHTML variable.
+ */
+const renderAllPlayers = (playerList) => {
+    try {
 
     } catch (err) {
         console.error('Uh oh, trouble rendering players!', err);
@@ -109,7 +134,7 @@ const removePlayer = async (playerId) => {
 /**
  * It renders a form to the DOM, and when the form is submitted, it adds a new player to the database,
  * fetches all players from the database, and renders them to the DOM.
-*/
+ */
 const renderNewPlayerForm = () => {
     try {
 
@@ -125,8 +150,24 @@ const init = async () => {
     renderNewPlayerForm();
 }
 
-// fetchAllPlayers();
-// fetchSinglePlayer(6478);
-// addNewPlayer();
-// removePlayer(6475);
-// init();
+init();
+
+
+
+
+
+
+// Notes on passing between multiple functions with arrow functions
+/*
+const doubleNumber = (number) => {
+    return number * 2;
+};
+
+// myResult will store 5 passed into doubleNumber function above and output the return
+const myResultForFive = doubleNumber(5);
+
+// log result
+console.log(myResultForFive);
+
+
+*/
